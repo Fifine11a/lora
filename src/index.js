a3868d644f989ec008e3b43e86b7e0642e86496d
MARIE_M <fifi.maresova@gmail.com>
2021-01-22 18:06:19 +0100

    Showing and hiding navigation on-click for smaller display

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import './index.html';
 import './style.css';
 
-console.log('funguju!');
+console.log('You better work, b*tch! ;)');
+
+const navBtn = document.getElementById('nav-btn');
+const navClosed = document.getElementById('nav-closed');
+const navItem = document.querySelectorAll('.nav-item');
+
+const showNaw = (e) => {
+  navClosed.classList.toggle('nav-closed');
+};
+
+navBtn.addEventListener('click', showNaw);
+
+navItem.forEach((item) => {
+  item.addEventListener('click', showNaw);
+});
