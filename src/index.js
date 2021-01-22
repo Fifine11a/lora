3592cb7ad5aae40838afec3672b23961fffe2d84
MARIE_M <fifi.maresova@gmail.com>
2021-01-22 18:49:38 +0100

    ordering button

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ console.log('You better work, b*tch! ;)');
 const navBtn = document.getElementById('nav-btn');
 const navClosed = document.getElementById('nav-closed');
 const navItem = document.querySelectorAll('.nav-item');
+const orderBtn = document.querySelector('.order-btn');
+const selectedDrink = document.querySelector('.drink__cup');
 
 const showNaw = (e) => {
   navClosed.classList.toggle('nav-closed');
@@ -16,3 +18,15 @@ navBtn.addEventListener('click', showNaw);
 navItem.forEach((item) => {
   item.addEventListener('click', showNaw);
 });
+
+const orderDrink = (e) => {
+  if (selectedDrink.classList.contains('drink__cup--selected')) {
+    selectedDrink.classList.toggle('drink__cup--selected');
+    orderBtn.textContent = 'Objednat';
+  } else {
+    selectedDrink.classList.toggle('drink__cup--selected');
+    orderBtn.textContent = 'Zrušit';
+  }
+};
+
+orderBtn.addEventListener('click', orderDrink);
