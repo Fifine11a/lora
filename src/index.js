ac8eef390b675986f1d219ba07911e9800612bbe
MARIE_M <fifi.maresova@gmail.com>
2021-01-23 20:08:51 +0100

    Component for drink layers

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const navClosed = document.getElementById('nav-closed');
 const navItem = document.querySelectorAll('.nav-item');
 const orderBtn = document.querySelector('.order-btn');
 const selectedDrink = document.querySelector('.drink__cup');
+const cappuccinoDrink = document.getElementById('cappuccino');
 
 const showNaw = (e) => {
   navClosed.classList.toggle('nav-closed');
@@ -30,3 +31,25 @@ const orderDrink = (e) => {
 };
 
 orderBtn.addEventListener('click', orderDrink);
+
+const Layer = (props) => {
+  return `
+<div class="layer">
+                  <div
+                    class="layer__color"
+                    style="background-color: ${props.color}"
+                  ></div>
+                  <div class="layer__label">${props.layer}</div>
+                </div>
+`;
+};
+
+const cappuccinoLayers = [
+  { color: '#feeeca', layer: 'mléčná pěna' },
+  { color: '#fed7b0', layer: 'teplé mléko' },
+  { color: '#613916', layer: 'espresso' },
+];
+
+cappuccinoLayers.forEach((layer) => {
+  cappuccinoDrink.innerHTML += Layer(layer);
+});
