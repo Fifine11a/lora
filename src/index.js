18e04e229ecb2cde485c17e3a399dbf40a67aefc
MARIE_M <fifi.maresova@gmail.com>
2021-02-05 18:55:25 +0100

    Drink component working

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ console.log('You better work, b*tch! ;)');
 const navBtn = document.getElementById('nav-btn');
 const navClosed = document.getElementById('nav-closed');
 const navItem = document.querySelectorAll('.nav-item');
-const orderBtn = document.querySelector('.order-btn');
-const selectedDrink = document.querySelector('.drink__cup');
-const cappuccinoDrink = document.getElementById('cappuccino');
 
 const showNaw = (e) => {
   navClosed.classList.toggle('nav-closed');
@@ -20,36 +17,98 @@ navItem.forEach((item) => {
   item.addEventListener('click', showNaw);
 });
 
-const orderDrink = (e) => {
-  if (selectedDrink.classList.contains('drink__cup--selected')) {
-    selectedDrink.classList.toggle('drink__cup--selected');
-    orderBtn.textContent = 'Objednat';
-  } else {
-    selectedDrink.classList.toggle('drink__cup--selected');
-    orderBtn.textContent = 'Zrušit';
-  }
-};
-
-orderBtn.addEventListener('click', orderDrink);
-
 const Layer = (props) => {
   return `
-<div class="layer">
-                  <div
-                    class="layer__color"
-                    style="background-color: ${props.color}"
-                  ></div>
-                  <div class="layer__label">${props.layer}</div>
-                </div>
+  <div class="layer">
+  <div class="layer__color" style="background-color: ${props.color}"></div>
+  <div class="layer__label">${props.label}</div>
+  </div>
 `;
 };
-
+/* 
 const cappuccinoLayers = [
-  { color: '#feeeca', layer: 'mléčná pěna' },
-  { color: '#fed7b0', layer: 'teplé mléko' },
-  { color: '#613916', layer: 'espresso' },
+  { color: '#feeeca', label: 'mléčná pěna' },
+  { color: '#fed7b0', label: 'teplé mléko' },
+  { color: '#613916', label: 'espresso' },
 ];
 
 cappuccinoLayers.forEach((layer) => {
   cappuccinoDrink.innerHTML += Layer(layer);
+}); */
+
+const Drink = (props) => {
+  const element = document.createElement('div');
+  element.className = 'drink';
+  element.innerHTML = `
+  <div class="drink__product">
+  <div class="drink__cup">
+    <img src="assets/cups/${props.id}.png" />
+  </div>
+  <div class="drink__info">
+    <h3>${props.name}</h3>
+    <div id="app">${props.layers.map((layer) => Layer(layer))}</div>
+    </div>
+    </div>
+    <div class="drink__controls">
+    <button class="order-btn">Objednat</button>
+  </div>`;
+
+  const orderBtn = element.querySelector('.order-btn');
+  const selectedDrink = element.querySelector('.drink__cup');
+
+  const orderDrink = (e) => {
+    if (selectedDrink.classList.contains('drink__cup--selected')) {
+      selectedDrink.classList.toggle('drink__cup--selected');
+      orderBtn.textContent = 'Objednat';
+    } else {
+      selectedDrink.classList.toggle('drink__cup--selected');
+      orderBtn.textContent = 'Zrušit';
+    }
+  };
+
+  orderBtn.addEventListener('click', orderDrink);
+
+  return element;
+};
+
+const drinks = [
+  {
+    id: 'cappuccino',
+    name: 'Cappuccino',
+    ordered: false,
+    layers: [
+      {
+        color: '#feeeca',
+        label: 'mléčná pěna',
+      },
+      {
+        color: '#fed7b0',
+        label: 'teplé mléko',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
+  },
+  {
+    id: 'romano',
+    name: 'Romano',
+    ordered: false,
+    layers: [
+      {
+        color: '#fbdf5b',
+        label: 'citrón',
+      },
+      {
+        color: '#613916',
+        label: 'espresso',
+      },
+    ],
+  },
+];
+
+const drinksList = document.querySelector('.drinks-list');
+drinks.forEach((drink) => {
+  drinksList.appendChild(Drink(drink));
 });
